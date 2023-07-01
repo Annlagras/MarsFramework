ccd40ff8fab3c818702006f6dff21b3576665837
Annlagras <annlagras@gmail.com>
2023-07-02 02:06:48 +1200

    Add project files.

---
+++ MarsFramework/Test/ShareSkillTest.cs
@@ -0,0 +1,33 @@
+﻿using MarsFramework.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsFramework.Test
+{
+    public class ShareSkillTest
+    {
+
+        [TestFixture, Description("this fixture contains Mars Framework")]
+       
+        class User : Global.Base
+        {
+
+            [Test, Description("Check if the user is able to add ShareSkill details successfully")]
+            public void EnterShareSkill()
+            {
+                //Create Extent Report
+                test = extent.StartTest("Add Share Skill");
+                //Add Share Skill
+                ShareSkill shareSkillObj = new ShareSkill();
+                shareSkillObj.EnterShareSkill();
+                shareSkillObj.VerifySkill();
+            }
+
+
+        }
+    }
+}
