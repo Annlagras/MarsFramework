747d2e21e902b5799be303bd357e9a142e0dcf55
Anna Joy <118405409+Annlagras@users.noreply.github.com>
2023-09-27 23:01:09 +1300

    Add files via upload

---
+++ MarsFramework/Test/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Competition.Tests
+{
+    internal class Tests : Global.Base
+    {
+        [Test]
+        public void WhenIEnterListing()
+        {
+            test = extent.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
+    }
+}
